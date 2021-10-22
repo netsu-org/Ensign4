c3b99aee9ad2199510eaad8e18e6d8623b413517
NETtoSan <netzamora05@gmail.com>
2021-10-22 09:21:42 +0100

    basic bootloader

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-console.log("h")
+const fs = require('fs')
+class load {
+    constructor(file){
+        this.tempFile = {}
+    }
+    async load(dir){
+        new(require(dir))(this)
+    }
+    async initialize(){
+        const dir = fs.readdirSync("./service")
+        for(const h of dir){
+            try{
+                await this.load(`./service/${h}`)
+            }
+            catch(err){
+                console.log(`Failed to load ${h} (${err})`)
+            }
+        }
+    }
+}
+
+new load().initialize()
+
+module.exports = load
