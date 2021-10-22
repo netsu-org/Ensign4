56d61a26de6efad9c4010a2d520b23a1cc0f9053
NETtoSan <netzamora05@gmail.com>
2021-10-22 10:42:55 +0100

    template

---
+++ system/auto/toxic.js
@@ -0,0 +1,13 @@
+require('@tensorflow/tfjs')
+const toxicity = require('@tensorflow-models/toxicity')
+const discord = require('discord.js')
+
+class toxic{
+    constructor(bot){
+        this.bot = bot
+        this.map = {}
+    }
+    async run(message)
+}
+
+module.exports = toxic
